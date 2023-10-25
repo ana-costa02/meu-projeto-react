49038e4e008b0562e3c2eeb0adc0e09a637455f8
Ana Carolina da Costa <110925785+ana-costa02@users.noreply.github.com>
2023-10-24 22:52:38 -0300

    Explorando JSX, Components e Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
+
+  const nome = "Ana";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SayMyName nome="Ana" />
+      <SayMyName nome="Maria" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Ana"
+        idade="31"
+        profissao="Programadora"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
---
+++ src/components/HelloWord.js
@@ -0,0 +1,13 @@
+import Frase from "./Frase";
+
+function HelloWorld() {
+  return (
+    <div>
+        <Frase/>
+        <h2>Meu primeiro componente</h2>
+        <Frase/>
+    </div>
+  )
+}
+
+export default HelloWorld;
